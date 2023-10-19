ca83a9a124368bcc2ce00d97c64173c299f298dd
ajvejko <moucka.matej01@gmail.com>
2023-10-19 22:07:31 +0200

    added footer

--- src/Components/Contact.vue
+++ src/Components/Contact.vue
@@ -54,5 +54,25 @@
         /></a>
       </div>
     </div>
+    <div
+      class="absolute bottom-0 left-0 right-0 flex justify-center border-t pb-1 pt-2 text-center text-sm text-textMainCustom lg:text-lg xl:text-xl"
+    >
+      Want to checkout the source code? ->
+      <a href="https://github.com/ajvejko/portfolio" alt="github" class="ml-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="h-6 w-6 stroke-textMainCustom md:h-8 md:w-8 lg:h-9 lg:w-9"
+        >
+          <path
+            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+          ></path>
+        </svg>
+      </a>
+    </div>
   </section>
 </template>
